2a81cb35181817dafe6d00e71dc83206fe89a3ab
mekbibDev <mekbibtare@gmail.com>
2023-12-26 14:03:19 +0300

    Display the game status

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
 import Cards from "./Cards";
+
 const url = "https://rickandmortyapi.com/api/character/?status=dead";
+const gameStatusMessages = {
+  ongoing: "Try to pick each card only once",
+  win: "You've won! Click a card to play again",
+  lose: "Sorry, Try to pick each card only once",
+};
+
 export default function App() {
   const [characters, setCharacters] = useState([]);
   const [pickedCards, setPickedCards] = useState([]);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [gameStatus, setGameStatus] = useState(gameStatusMessages.ongoing);
 
   useEffect(() => {
     fetch(url)
@@ -38,16 +46,24 @@ export default function App() {
       ? 0
       : score + 1;
     const newBestScore = newScore > bestScore ? newScore : bestScore;
+    const newGameStatus =
+      newScore === characters.length
+        ? gameStatusMessages.win
+        : newScore === 0 && gameStatus !== gameStatusMessages.win
+          ? gameStatusMessages.lose
+          : gameStatusMessages.ongoing;
 
     setPickedCards(updatedPicked);
     setScore(newScore);
     setBestScore(newBestScore);
+    setGameStatus(newGameStatus);
   }
   if (characters.length !== 0)
     return (
       <>
         <h1>Score:{score}</h1>
         <h1>Best Score: {bestScore}</h1>
+        <h2>{gameStatus}</h2>
         <Cards
           characters={characters}
           handleCardClick={handleCardClick}
