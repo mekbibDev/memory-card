3ed898827292a88ef3c18c930ca0c4d459ee605d
mekbibDev <mekbibtare@gmail.com>
2023-12-26 10:23:31 +0300

    Fetch characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { useEffect, useState } from "react";
+const url = "https://rickandmortyapi.com/api/character/?status=dead";
 export default function App() {
+  const [characters, setCharacters] = useState([]);
 
-  return (
-    <>
- 
-    </>
-  );
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((response) => {
+        setCharacters(response.results);
+      });
+  }, []);
+  return <></>;
 }
