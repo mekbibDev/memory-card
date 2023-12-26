85a8ebe217f6260ecd8e3d213b4d74b9bee4de8d
mekbibDev <mekbibtare@gmail.com>
2023-12-26 15:24:51 +0300

    Fix alt redundancy

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ name, imgUrl, characterId,handleCardClick, }) {
   return (
     <div onClick={handleCardClick} >
-      <img src={imgUrl} alt="name" data-characterid={characterId}/>
+      <img src={imgUrl} alt="" data-characterid={characterId}/>
       <h1 data-characterid={characterId}>{name}</h1>
       <button onClick={handleCardClick} data-characterid ={characterId} >pick</button>
     </div>
